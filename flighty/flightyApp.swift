2388e3b7dede2a7031a5e8849da62ac73b716f09
Chris Free <chris@chromatichq.com>
2024-04-12 09:08:24 -0400

    Initial Commit

---
+++ flighty/flightyApp.swift
@@ -0,0 +1,17 @@
+//
+//  flightyApp.swift
+//  flighty
+//
+//  Created by Christopher Free on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct flightyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
