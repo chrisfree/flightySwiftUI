188e331fa23365ce716c410aaf21163a8fd21d6f
Chris Free <chris@chromatichq.com>
2024-04-23 10:36:50 -0500

    Adds Booking/Seat and Good to Know sections.

---
+++ flighty/Views/BookingAndSeatDetails.swift
@@ -0,0 +1,18 @@
+//
+//  BookingAndSeatDetails.swift
+//  flighty
+//
+//  Created by Christopher Free on 4/22/24.
+//
+
+import SwiftUI
+
+struct BookingAndSeatDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookingAndSeatDetails()
+}
---
+++ flighty/Views/GoodToKnowSection.swift
@@ -0,0 +1,18 @@
+//
+//  GoodToKnowSection.swift
+//  flighty
+//
+//  Created by Christopher Free on 4/22/24.
+//
+
+import SwiftUI
+
+struct GoodToKnowSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoodToKnowSection()
+}
