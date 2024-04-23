9191a9854d01c1d0273feb0581d0b7b344e409c3
Chris Free <chris@chromatichq.com>
2024-04-23 17:21:27 -0500

    Updating some colors for fine tuning the UI.

---
+++ flighty/Views/ArrivalForecast.swift
@@ -0,0 +1,18 @@
+//
+//  ArrivalForecast.swift
+//  flighty
+//
+//  Created by Christopher Free on 4/23/24.
+//
+
+import SwiftUI
+
+struct ArrivalForecast: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArrivalForecast()
+}
