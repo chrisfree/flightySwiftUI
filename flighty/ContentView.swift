cb80c8689bee066ff05fa9cc5c48a1de7cc2b4f5
Chris Free <chris@chrisfree.com>
2024-05-02 16:08:58 -0500

    Pushing the Map a bit further.
    * Adds an `AirportAnnotation` struct for encapsulating details for each map annotation.
    * Extends `CLLocationCoordinate2D` with some handy static coordinates.
    * Updates the default `PresentationDetent` to be in its smallest/"closed" state.
    * Include some logic to show/hide the close button based on the state of the presented Flight Details sheet.
    * Builds out the basics of the Map, including annotations for departure and arrival airports and mock flight path.
    * Makes the Map interactive while the `sheet` is presented using `.presentationBackgroundInteraction().`

--- flighty/ContentView.swift
+++ flighty/ContentView.swift
@@ -12,11 +12,53 @@ struct ContentView: View {
     @StateObject var uiModel = UIModel()
     @State private var sheetPresented: Bool = true
     @State private var referenceOpacity = 0.0
+    @State private var camera: MapCameraPosition = .camera(MapCamera(centerCoordinate: CLLocationCoordinate2D(latitude: 35.44722362595532, longitude: -86.03197387024086), distance: 5_000_000))
+
+    let locations: [AirportAnnotation] = [
+        AirportAnnotation(code: "ATL", name: "Atlanta", coordinate: .atl),
+        AirportAnnotation(code: "ORD", name: "Chicago", coordinate: .ord)
+    ]
+
+    let flightRoute: [CLLocationCoordinate2D] = [.atl, .midPoint, .ord]
 
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
-            Map {
+            Map(position: $camera) {
+                ForEach(locations) { location in
+                    Annotation("\(location.name)", coordinate: location.coordinate, anchor: .leading) {
+                        HStack(spacing: 3) {
+                            Circle()
+                                .fill(.flightyBlue)
+                                .stroke(.white, lineWidth: 2)
+                                .frame(maxWidth: 15)
 
+                            HStack(spacing: 0) {
+                                Text(location.code)
+                                    .font(.system(size: 10).weight(.semibold))
+                                    .foregroundStyle(.white)
+                                    .padding(.leading, 4)
+                                    .padding(.trailing, 3)
+                                    .frame(maxHeight: .infinity, alignment: .center)
+                                    .background(.flightyBlue)
+
+                                Text(location.name)
+                                    .font(.system(size: 12))
+                                    .foregroundStyle(.white)
+                                    .padding(.vertical, 3)
+                                    .padding(.leading, 3)
+                                    .padding(.trailing, 4)
+                                    .background(.flightyLightBlue)
+
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                            .fixedSize()
+                        }
+                        .offset(x: -4)
+                    }
+
+                    MapPolyline(coordinates: flightRoute)
+                        .stroke(.blue, lineWidth: 3)
+                }
             }
             .ignoresSafeArea()
 
@@ -38,6 +80,9 @@ struct ContentView: View {
             .sheet(isPresented: $sheetPresented) {
                 FlightDetails(sheetPresented: $sheetPresented)
                     .presentationDetents([.height(200), .medium, .fraction(0.95)], selection: $uiModel.selectedDetent)
+                    .presentationBackgroundInteraction(
+                        .enabled(upThrough: .medium)
+                    )
                     .presentationDragIndicator(.hidden)
                     .presentationCornerRadius(21)
                     .interactiveDismissDisabled()
@@ -73,4 +118,18 @@ struct ViewOffsetKey: PreferenceKey {
     static func reduce(value: inout Value, nextValue: () -> Value) {
         value += nextValue()
     }
+}
+
+struct AirportAnnotation: Identifiable {
+    let id = UUID()
+    let code: String
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+extension CLLocationCoordinate2D {
+    static let atl = CLLocationCoordinate2D(latitude: 33.640411, longitude: -84.419853)
+    static let ord = CLLocationCoordinate2D(latitude: 41.978611, longitude: -87.904724)
+    static let midPoint = CLLocationCoordinate2D(latitude: 39.06903755242377, longitude: -86.79896452319443)
+    static let overLake = CLLocationCoordinate2D(latitude: 42.03559217412583, longitude: -86.82568547736001)
 }
